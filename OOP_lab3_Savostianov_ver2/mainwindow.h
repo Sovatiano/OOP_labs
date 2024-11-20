edff1a7b05c1fb0370b90678250fe6ffe9fc29ae
Владислав Савостьянов <savostianov@outlook.com>
2024-11-20 03:19:44 +0300

    Updated mainwindow

--- OOP_lab3_Savostianov_ver2/mainwindow.h
+++ OOP_lab3_Savostianov_ver2/mainwindow.h
@@ -1,12 +1,10 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QPainter>
+#include "Department_Savostianov.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,7 +15,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onLoad();
+    void onSave();
+    void onClear();
+    void onPaint();
+
 private:
     Ui::MainWindow *ui;
+    Department department;
 };
-#endif // MAINWINDOW_H
